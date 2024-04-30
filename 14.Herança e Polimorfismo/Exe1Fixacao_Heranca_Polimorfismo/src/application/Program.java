c13cd22fde1fb738b3eb4194d5d98ea169b02a85
Karine Antoniacomi <karine.antoniacomi@gmail.com>
2024-04-30 11:59:13 -0300

    Adiciona enunciado do exercício

--- 14.Herança e Polimorfismo/Exe1Fixacao_Heranca_Polimorfismo/src/application/Program.java
+++ 14.Herança e Polimorfismo/Exe1Fixacao_Heranca_Polimorfismo/src/application/Program.java
@@ -11,6 +11,18 @@ import entities.ImportedProduct;
 import entities.Product;
 import entities.UsedProduct;
 
+/* Fazer um programa para ler os dados de N produtos (N fornecido 
+pelo usuário). Ao final, mostrar a etiqueta de preço de cada 
+produto na mesma ordem em que foram digitados.
+Todo produto possui nome e preço. Produtos importados possuem uma 
+taxa de alfândega, e produtos usados possuem data de fabricação.
+Estes dados específicos devem ser acrescentados na etiqueta de 
+preço conforme exemplo (próxima página). Para produtos
+importados, a taxa e alfândega deve ser acrescentada ao preço 
+final do produto. Favor implementar o programa conforme
+projeto ao lado.
+*/
+
 public class Program {
 
 	public static void main(String args[]){
