f304322d9a17fe5626c1e02a39c7b9f41c80f076
Karine Antoniacomi <karine.antoniacomi@gmail.com>
2024-09-27 17:13:55 -0300

    refactor: Abrindo e fechando Stream com try with resources

--- 16.TrabalhandoComArquivos/Arquivos/src/exemplosFileReaderBufferedReader/Program.java
+++ 16.TrabalhandoComArquivos/Arquivos/src/exemplosFileReaderBufferedReader/Program.java
@@ -4,44 +4,24 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-// Processo de abrir e fechar manualmente Stream
+// Bloco try with resources para abrir e fechar as streams 
+// sem precisar fazer o processo manualmente
 public class Program {
 
 	public static void main(String[] args) {
 
-		String path = "C:\\Users\\karin\\AppData\\Local\\Temp\\ina.txt";
+		String path = "C:\\Users\\karin\\AppData\\Local\\Temp\\in.txt";
 
-		FileReader fr = null;
-		BufferedReader br = null;
-
-		try {
-			// Stream - sequencia de leitura
-			fr = new FileReader(path);
-			// BufferedReader - oega a stream básica FileReader e a partir dela
-			// instancia a stream que tem o buffer p/ deixar mais rapida a leitura
-			br = new BufferedReader(fr);
-
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 			String line = br.readLine(); // lê uma linha do arquivo, se estiver no final retorna nulo
+			
 			while (line != null) {
 				System.out.println(line);
 				line = br.readLine();
 			}
-		} catch (IOException e) {
+		} 
+		catch (IOException e) {
 			System.out.println("Error: " + e.getLocalizedMessage());
-		}
-		// finally p/ fechar as duas streams
-		finally {
-			// bloco try pois também pode ocorrer exceção na hora de fechar as streams
-			try {
-				if (br != null) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 	}
 }
