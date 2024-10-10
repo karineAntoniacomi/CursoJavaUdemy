b6d6492140a7e801af3e9fe51c9c0ca14cab6f3e
Karine Antoniacomi <karine.antoniacomi@gmail.com>
2024-10-10 18:41:37 -0300

    feat: interfaces, resolvendo problema inicial sem uso de interface

---
+++ 17.Interfaces/Interfaces/src/model/services/RentalService.java
@@ -0,0 +1,45 @@
+package model.services;
+
+import java.time.Duration;
+
+import Exemplo1.model.entities.CarRental;
+import Exemplo1.model.entities.Invoice;
+
+public class RentalService {
+
+	private Double pricePerHour;
+	private Double pricePerDay;
+	
+	private BrazilTaxService taxService;
+
+	public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService taxService) {
+		this.pricePerHour = pricePerHour;
+		this.pricePerDay = pricePerDay;
+		this.taxService = taxService;
+	}
+	
+	// método que gera a fatura a parir do carRental
+	public void processInvoice(CarRental carRental) {
+		
+		// obtendo as horas e minutos para arredondar as horas p/ cima
+		double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+		double hours = minutes / 60.0;
+				
+		double basicPayment;
+		if (hours <= 12.0) {
+			// Arredondando hora p/ cima -> função ceil / teto
+			basicPayment = pricePerHour * Math.ceil(hours);
+		}
+		else {
+			// preço por dia caso horas > 12
+			basicPayment = pricePerDay * Math.ceil(hours / 24.0);
+		}
+		
+		
+		// Imposto
+		double tax = taxService.tax(basicPayment);
+		
+		carRental.setInvoice(new Invoice(basicPayment, tax));
+	}
+	
+}
